d39e0f2dbcabff312c2d31cf3cf20bed361bc300
eunseon <eunseon@localhost>
2025-08-08 11:25:43 +0900

    commit

---
+++ CodeON/src/main/java/com/spring/app/controller/FrontController.java
@@ -0,0 +1,17 @@
+package com.spring.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class FrontController {
+	
+	@GetMapping("CodeON")
+	public String start() {
+		return "test";
+	}
+	
+	
+}
